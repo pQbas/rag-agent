6abc365a8158f4bfd65390ddf3093b84bf7303b7
pQbas <percybrayan18@gmail.com>
2025-04-04 16:52:24 -0500

    update chain

--- src/agent/classes/chain.py
+++ src/agent/classes/chain.py
@@ -1,35 +1,40 @@
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 from pydantic import Field, create_model
-
-
 from dotenv import load_dotenv
 
 
 load_dotenv()
 
 class Chain:
-    def __init__(self, llm, system, human, output):
+    def __init__(self, llm, system, output = None, human = None):
         self.llm = llm
         self.system = system
-        self.human = human
 
-        self.prompt_scheme =  ChatPromptTemplate.from_messages(
-            [
-                ("system", self.system),
-                ("human",  self.human ),
-            ]
-        )
+        if human is not None:
+            self.human = human
 
-        self.output_type, self.output_description = output
+            self.prompt_scheme =  ChatPromptTemplate.from_messages(
+                [
+                    ("system", self.system),
+                    ("human",  self.human ),
+                ]
+            )
 
-        self.output_class = create_model(
-            'OutputModel',
-            score=(self.output_type, Field(description = self.output_description)) 
-        )
+            self.output_type, self.output_description = output
 
-        self.structured_output = llm.with_structured_output(self.output_class)
+            self.output_class = create_model(
+                'OutputModel',
+                score=(self.output_type, Field(description = self.output_description)) 
+            )
 
-        self.chain = self.prompt_scheme | self.structured_output 
+            self.structured_output = llm.with_structured_output(self.output_class)
+
+            self.chain = self.prompt_scheme | self.structured_output 
+
+        else:
+            self.chain = self.system | llm | StrOutputParser()
+
 
     def invoke(self, input):
         return self.chain.invoke(input)
