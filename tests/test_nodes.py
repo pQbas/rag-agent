f3ecf5b3f117fdb66bac58b2ab786ae116fd520a
pQbas <percybrayan18@gmail.com>
2025-04-04 16:52:03 -0500

    update tests

--- tests/test_nodes.py
+++ tests/test_nodes.py
@@ -1,21 +1,46 @@
 from src.agent.graph import Evaluate, GenerateAnswer, AnyDoc, Retriever, GraphState
+from langchain_core.documents import Document
 
 
-def test_generate_answer():
+DOCUMENTS = [
+        Document(page_content="AI is revolutionizing various fields, especially Coding with tools like Cursor."),
+        Document(page_content="AI is also advancing in autonomous vehicles"),
+        Document(page_content="AI is also advancing in healthcare innovations")
+        ]
 
-    generate_node = GenerateAnswer() 
+QUESTION = "What are the latest trends in AI?"
 
-    graph_state_instance = GraphState(
-        question="What are the latest trends in AI?",
-        solution="AI is evolving in areas like natural language processing and reinforcement learning.",
-        online_search=False,
-        documents=["AI is a revolution in dfiferent areas of technology, one of them is Coding, Cursor is the best tool",
-                   "Monkeys are like humans",
-                   "A type of Pokemon is Charizard"]
-    )
+SOLUTION = "The latest trends in AI include advancements in coding tools like Cursor, autonomous vehicles, and healthcare innovations."
 
-    response = generate_node.invoke(graph_state_instance) 
+
+def test_generate_answer_node():
+
+    generate_node = GenerateAnswer()
+    graph_state_instance = GraphState(question = QUESTION, documents = DOCUMENTS)
+
+    response = generate_node.invoke(graph_state_instance)
+
+    print(response.get('solution'))
 
     assert type(response) is dict 
 
+
+
+def test_evaluate_node():
+
+    evaluate_node = Evaluate()
+    graph_state_instance = GraphState(question = QUESTION, documents = DOCUMENTS)
+
+    response = evaluate_node.invoke(graph_state_instance)
+
+    assert type(response) is dict 
+
+
+def test_any_doc_node():
+    any_doc_node = AnyDoc()
+    graph_state_instance = GraphState(question = QUESTION, documents = DOCUMENTS, solution = SOLUTION)
+
+    response = any_doc_node.invoke(graph_state_instance)
+
+    assert response in ["Answers Question", "Question not adressed", "Hallucinations detected"]
 
